4b659248d2de4e31c03975cd29e455d212701241
RAJ <vermar4142@gmail.com>
2020-08-23 19:50:33 +0530

    Added:- The functionality to start with fresh board

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -1,2 +1,13 @@
 #!/bin/bash 
 echo -e "Welcome To The Tic Tac Toe Game Program Hope You Win From Computer \n"
+
+#As a Player starting fresh with resetting the board(UC1).
+function resetBoard(){
+	echo "====================="
+	echo "Game started"
+	resetBoard=(_ _ _ _ _ _ _ _ _)
+	echo "====================="  
+}
+
+#Main Code
+resetBoard
