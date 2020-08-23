2c5be5280d9faf99de4b8cefba78b1c55b580c67
RAJ <vermar4142@gmail.com>
2020-08-23 22:26:24 +0530

    Added:- The functionality to check winner,tie or change the turn

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -10,12 +10,15 @@ function resetBoard(){
 	echo "    Game started"
 	playBoard=(_ _ _ _ _ _ _ _ _)
 	echo "====================="  
-
+	
+	tossPlay
+	printBoard
+	playGame
 }
 
-#As a Player would like to see the board so player can choose the valid cells during his turn
+#As a Player would like to see the board so player can choose the valid cells during his turn(UC4).
 function printBoard(){
-	echo "****Tic Tac Toe****"
+	echo -e "\n****Tic Tac Toe****"
 	echo  "  ---------"
 	echo  "|  " ${playBoard[0]} ${playBoard[1]} ${playBoard[2]} "  |" 
 	echo  "  ---------"
@@ -42,14 +45,15 @@ function tossPlay(){
 	then
 		echo -e "Its The Player Turn \n"	
 		read -p "Choose What You Want X Or O : " playerChoice
-		if [[ "$playerChoice" == "X" || "$playerChoice" == "x" ]]
+		if [[ "$playerChoice" == X || "$playerChoice" == x ]]
 		then 
 			computerChoice=O
-		elif [[ "$playerChoice" == "O" || "$playerChoice" == "o" ]]
+		elif [[ "$playerChoice" == O || "$playerChoice" == o ]]
 		then 
 			computerChoice=X
 		else
-			echo "Enter The Correct Letter"
+			echo -e "\nYou Have Entered The InCorrect Letter Your Chance Is Gone Tossing Again"
+			tossPlay
 		fi
 	echo -e "\nPlayer Choose The Option : $playerChoice \n"
 	echo -e "Computer Choose the Option : $computerChoice \n"		
@@ -73,8 +77,10 @@ function tossPlay(){
 	fi			
 }
 
+#To Change The Turns After Every Move
 function playerTurnsChange(){
 	i=$1
+	checkWinner
 	tossCheck=$i
 
 	if [ $totalMovesLeft -eq 0 ]
@@ -85,6 +91,7 @@ function playerTurnsChange(){
 	fi
 }
 
+#To Play The Game Between Computer And Player
 function playGame(){
 	if [ $tossCheck -eq  1 ]
 	then
@@ -121,10 +128,65 @@ function playGame(){
 	
 }
 
-#Main Code
+#Determining after every move the winner or the tie or change the turn(UC5).
+function checkBoard(){
+	i1=$1
+	i2=$2
+	i3=$3
 
+	if [ "${playBoard[$i1]}" != "_" ]
+	then
+		if [[ "${playBoard[$i1]}" == "${playBoard[$i2]}" && "${playBoard[$i2]}" == "${playBoard[$i3]}" && "${playBoard[$i3]}" == "${playBoard[$i1]}" ]]
+		then
+			winnerPlayer=${playBoard[$i1]} 
+			if [ "$winnerPlayer" ==  "$playerChoice" ]
+			then
+				echo -e "\nPlayer With Letter $winnerPlayer Wins The Game "
+				echo -e "\nDo You Want To Play Again"
+				while true
+				do
+					echo "1.Play Again"
+					echo "2.Exit"
+					read -p "Enter Your Choice : " choice
+					case $choice in
+					"1") resetBoard ;;	
+					"2") exit ;;
+					esac	
+				done
+			else
+				echo "Computer With Letter $WinnerPlayer Wins The Game "
+				computerPlay=$(($RANDOM % 2 + 1))
+				if [ $computerPlay -eq 1 ]
+				then
+					echo "Computer Wants To Play Again"
+					resetBoard
+				else
+					exit
+				fi
+			fi
+		elif [ $totalMovesLeft -eq 0 ]
+		then
+			echo -e "\nGame Ties Between The Player And Computer"
+		fi		
+	fi
+}
+
+#Checking The Winner After Every Move(UC5).
+function checkWinner(){
+	if [ $totalMovesLeft -lt 5 ]
+	then
+		checkBoard 0 1 2
+		checkBoard 3 4 5
+		checkBoard 6 7 8
+		checkBoard 0 4 8
+		checkBoard 2 4 6
+		checkBoard 0 3 6
+		checkBoard 1 4 7
+		checkBoard 2 5 8	
+	fi
+}
+
+#Main Code
 resetBoard
-tossPlay
-printBoard
-playGame
+
 
