10a3bf596f497ae1057663731779577087ea9a44
RAJ <vermar4142@gmail.com>
2020-08-23 19:59:39 +0530

    Added:- The functionality to check who plays first

--- TicTacToeGame.sh
+++ TicTacToeGame.sh
@@ -9,5 +9,21 @@ function resetBoard(){
 	echo "====================="  
 }
 
+#As a Player begin with a toss to check who plays first(UC2).
+function tossPlay(){
+	tossCheck=$(($RANDOM % 2 + 1))	
+	if [ $tossCheck -eq 1 ]
+	then
+		echo -e "Its The Player Turn \n"	
+		
+	fi
+	
+	if [ $tossCheck -eq 2 ]
+	then
+		echo -e "Its The Computer Turn \n"
+	fi			
+}
+
 #Main Code
+tossPlay
 resetBoard
